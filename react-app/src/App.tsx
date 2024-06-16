046dff6fe6e3ee1a31b4bcb6c7588987ae8be548
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-16 08:48:07 -0400

    Using plan/vanilla css

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -7,10 +7,10 @@ import Button from "./components/Button";
 
 function App() {
 
-  // let items = ["New York", "San Francisco", "Tokyo", "Londo", "Paris"];
-  // const handleSelecetItem = (item:string) =>{
-  //   console.log(item)
-  // }
+  let items = ["New York", "San Francisco", "Tokyo", "Londo", "Paris"];
+  const handleSelecetItem = (item:string) =>{
+    console.log(item)
+  }
   const [alert,setAlert] = useState(false);
   const [color,setColor] = useState('warning')
   const colors = ['primary', 'secondary', 'success', 'warning', 'dark'];
@@ -20,14 +20,14 @@ function App() {
     setColor(colors[Math.floor(Math.random() * n)])
   }
   return (
-  //  <ListGroup items= {items} heading = "Cities" onSelectItem = {handleSelecetItem}/>
+  //  
   <>
-{alert && <Alert onClose={() => setAlert(false)}>
+{/* {alert && <Alert onClose={() => setAlert(false)}>
 My Alert
 </Alert>}
   
-   <Button  color={color} onClick={()=>setAlert(true)}>Success </Button>
-   
+   <Button  color={color} onClick={()=>setAlert(true)}>Success </Button> */}
+   <ListGroup items= {items} heading = "Cities" onSelectItem = {handleSelecetItem}/>
   </>
   );
 }
--- react-app/src/components/ListGroup/ListGroup.tsx
+++ react-app/src/components/ListGroup/ListGroup.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-
+import './ListGroup.css'
 interface Props {
     items: string[],
     heading:string;
