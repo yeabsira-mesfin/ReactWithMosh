3c533eca756dd24247ba3f86d558d84ae383f39c
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-22 14:04:10 -0400

    is valid

--- forms/src/FromsUsingStates.tsx
+++ forms/src/FromsUsingStates.tsx
@@ -4,12 +4,12 @@ import { zodResolver } from "@hookform/resolvers/zod/dist/zod.js";
 
 const schema = z.object({
   name:z.string().min(3,{message: 'Name must be at least three characters'}),
-  age:z.number({invalid_type_error:'Age field is required'}).min(18)
+  age:z.number({invalid_type_error:'Age field is required'}).min(18,{message:'Age must be at least 18'})
 })
 type FormData = z.infer<typeof schema>;
 const FromsUsingStates = () => {
 
-  const { register,handleSubmit, formState:{errors} } = useForm<FormData>({resolver:zodResolver(schema)});
+  const { register,handleSubmit, formState:{errors,isValid} } = useForm<FormData>({resolver:zodResolver(schema)});
 
 
   function onSubmit(data:FieldValues){
@@ -44,7 +44,7 @@ const FromsUsingStates = () => {
         />
          {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
-      <button className="btn btn-primary">Submit</button>
+      <button disabled={!isValid} className="btn btn-primary">Submit</button>
     </form>
   );
 };
