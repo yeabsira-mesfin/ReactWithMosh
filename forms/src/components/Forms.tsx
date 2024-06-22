97ea3f85651230cf4fdc76e3efbf2f453eebf265
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-22 10:09:25 -0400

    Form submission

--- forms/src/components/Forms.tsx
+++ forms/src/components/Forms.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 
 const Forms = () => {
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log("Submitted")
+    }
+    
   return (
-    <form action="">
+   
+    <form action="" onSubmit={handleSubmit}>
+       
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">Name</label>
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
         <input id="name" type="text" className="form-control" />
       </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">Age</label>
+        <input id="age" type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary">Submit</button>
     </form>
   );
 };
