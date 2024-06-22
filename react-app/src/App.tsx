1d96af6804b4c922b820f2bbfa88feac8eb40c7f
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-22 09:41:56 -0400

    More/Less Exercise

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,37 +3,58 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
-import Button from "./components/Button";
-import {BsFillCalendarFill} from 'react-icons/bs'
+// import Button from "./components/Button";
+import { BsFillCalendarFill } from "react-icons/bs";
 import ButtonExercise from "./components/ButtonExercise";
+import style from "styled-components";
+const Button = style.button `
+background-color: green;
+height: 2.5rem;
+border-radius: 3rem;
+border:none;
 
+
+&:hover {
+color: white;
+
+}
+`
 function App() {
+  // let items = ["New York", "San Francisco", "Tokyo", "Londo", "Paris"];
+  // const handleSelecetItem = (item:string) =>{
+  //   console.log(item)
+  // }
+  // const [alert,setAlert] = useState(false);
+  // const [color,setColor] = useState('warning')
+  // const colors = ['primary', 'secondary', 'success', 'warning', 'dark'];
+  // const n = colors.length;
+  // function handleColor() {
 
-  let items = ["New York", "San Francisco", "Tokyo", "Londo", "Paris"];
-  const handleSelecetItem = (item:string) =>{
-    console.log(item)
-  }
-  const [alert,setAlert] = useState(false);
-  const [color,setColor] = useState('warning')
-  const colors = ['primary', 'secondary', 'success', 'warning', 'dark'];
-  const n = colors.length;
-  function handleColor() {
+  //   setColor(colors[Math.floor(Math.random() * n)])
+  // }
+
+  const [drink, setDrink] = useState({ title: "Buna", price: 10 });
+  function handleClick (){
+ 
     
-    setColor(colors[Math.floor(Math.random() * n)])
+    setDrink({...drink, price:13});
   }
   return (
-  //  
-  <>
-{/* {alert && <Alert onClose={() => setAlert(false)}>
+    //
+    <>
+      {/* {alert && <Alert onClose={() => setAlert(false)}>
 My Alert
 </Alert>}
    
    <Button  color={color} onClick={()=>setAlert(true)}>Success </Button> */}
-   <BsFillCalendarFill/>
+      {/* <BsFillCalendarFill/>
    <ListGroup items= {items} heading = "Cities" onSelectItem = {handleSelecetItem}/>
 
-   <ButtonExercise/>
-  </>
+   <ButtonExercise/> */}
+   {drink.price}
+   <Button onClick ={handleClick}>Increase Price</Button>
+
+    </>
   );
 }
 
