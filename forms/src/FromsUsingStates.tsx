f43c88f958809ab4e45df17825580746c025459b
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-22 11:02:20 -0400

    form using react hook form

--- forms/src/FromsUsingStates.tsx
+++ forms/src/FromsUsingStates.tsx
@@ -1,32 +1,42 @@
-import React, { FormEvent, useState } from 'react'
-import { FormState } from 'react-hook-form';
+import React, { FormEvent, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const FromsUsingStates = () => {
-    const [person,setPerson] = useState({
-        name:'',
-        age:''
-    })
 
-function handleSubmit(event:FormEvent){
-    event.preventDefault();
-    console.log(person)
-}
+  const { register,handleSubmit } = useForm();
+
+  function onSubmit(data:FieldValues){
+    
+    console.log(data)
+  }
+
   return (
-    <form action="" onSubmit={handleSubmit}>
-       
-    <div className="mb-3">
-      <label htmlFor="name" className="form-label">
-        Name
-      </label>
-      <input value={person.name} onChange={(event)=>{setPerson({...person, name: event.target.value})}}  id="name" type="text" className="form-control" />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="age" className="form-label">Age</label>
-      <input value={person.age} onChange={(event)=>{setPerson({...person, age:event.target.value})}}  id="age" type="number" className="form-control" />
-    </div>
-    <button className="btn btn-primary">Submit</button>
-  </form>
-  )
-}
+    <form action="" onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          {...register('name')}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          {...register('age')}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </form>
+  );
+};
 
-export default FromsUsingStates
+export default FromsUsingStates;
