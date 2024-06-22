8a27c7f27f94ac2d37a270f132eb55ffc61b56bf
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-22 12:06:16 -0400

    Format resolution using Zod

--- forms/src/FromsUsingStates.tsx
+++ forms/src/FromsUsingStates.tsx
@@ -1,9 +1,16 @@
-import React, { FormEvent, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import {z} from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod/dist/zod.js";
 
+const schema = z.object({
+  name:z.string().min(3,{message: 'Name must be at least three characters'}),
+  age:z.number({invalid_type_error:'Age field is required'}).min(18)
+})
+type FormData = z.infer<typeof schema>;
 const FromsUsingStates = () => {
 
-  const { register,handleSubmit } = useForm();
+  const { register,handleSubmit, formState:{errors} } = useForm<FormData>({resolver:zodResolver(schema)});
+
 
   function onSubmit(data:FieldValues){
     
@@ -22,17 +29,20 @@ const FromsUsingStates = () => {
           type="text"
           className="form-control"
         />
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
+       
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          {...register('age')}
+          {...register('age',{valueAsNumber:true})}
           id="age"
           type="number"
           className="form-control"
         />
+         {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
